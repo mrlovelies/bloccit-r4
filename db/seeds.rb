4de92c7e2bdd7801613b0929e594384ff4e06446
PSDoff <alex@psdoff.com>
2014-03-20 22:00:04 -0400

    Associate users and posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,31 @@
 require 'faker'
 
-24.times do
-	p = Post.create(title: Faker::Lorem.sentence, body: Faker::Lorem.paragraph)
-	3.times do
-		p.comments.create(body: Faker::Lorem.paragraph)
+#Create 5 users with posts
+5.times do
+	password = Faker::Lorem.characters(10)
+	user = User.new(
+		name: Faker::Name.name,
+		email: Faker::Internet.email,
+		password: password,
+		password_confirmation: password)
+	user.skip_confirmation!
+	user.save
+
+	5.times do
+		post = Post.create(
+			user: user,
+			title: Faker::Lorem.sentence,
+			body: Faker::Lorem.paragraph)
+		# Set date created within the last year
+		post.update_attribute(:created_at, Time.now - rand(600..31536000))
 	end
 end
 
+user = User.first
+user.skip_reconfirmation!
+user.update_attributes(email: 'alex@psdoff.com', password: 'helloworld', password_confirmation: 'helloworld')
+
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
