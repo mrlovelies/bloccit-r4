bd0f04adfb685e9421c39a2f81b5f92e7553e654
PSDoff <alex@psdoff.com>
2014-03-23 01:06:02 -0400

    Update to previous commit. TODO: Fix error with topic validation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,7 +18,7 @@ class PostsController < ApplicationController
 
     if @post.save
       flash[:notice] = "Post was saved."
-      redirect_to @post
+      redirect_to [@topic, @post]
     else
       flash[:error] = "There was an error saving the post. Please try again."
       render :new
@@ -37,7 +37,7 @@ class PostsController < ApplicationController
     authorize @post
     if @post.update_attributes(post_params)
     	flash[:notice] = "Post was saved."
-    	redirect_to @post
+    	redirect_to [@topic, @post]
     else
     	flash[:error] = "There was an error saving the post. Please try again."
     	render :edit
--- app/models/post.rb
+++ app/models/post.rb
@@ -7,6 +7,6 @@ class Post < ActiveRecord::Base
 
 	validates :title, length: { minimum: 5 }, presence: true
 	validates :body, length: { minimum: 20 }, presence: true
-	validates :topic, presence: true
+	# validates :topic_id, presence: true
 	validates :user, presence: true
 end
