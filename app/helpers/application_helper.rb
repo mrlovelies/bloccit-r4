a1e1f83cf5b07a34dc5cbc68daadfa49cc3890e2
PSDoff <alex@psdoff.com>
2014-03-23 00:47:12 -0400

    Add validation methods to post, refactor post view for partials and errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+	def form_group_tag(errors, &block)
+		if errors.any?
+			content_tag :div, capture(&block), class: 'form-group error'
+		else
+			content_tag :div, capture(&block), class: 'form-group'
+		end
+	end
 end
