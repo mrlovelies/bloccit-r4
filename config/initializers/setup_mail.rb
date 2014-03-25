9dca018f733bed57f8fcb3731740669475e3db72
PSDoff <alex@psdoff.com>
2014-03-25 13:42:26 -0400

    Setup AWS, refactor credentials handling, incorporat avatar into views

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -4,8 +4,8 @@ if Rails.env.development?
 		address:        'smtp.sendgrid.net',
 		port:           '587',
 		authentication: :plain,
-		user_name:      "app23181839@heroku.com",
-		password:       "hcc4ens5",
+		user_name:      ENV['SENDGRID_USERNAME'],
+		password:       ENV['SENDGRID_PASSWORD'],
 		domain:         'heroku.com',
 		enable_starttls_auto: true
 	}
